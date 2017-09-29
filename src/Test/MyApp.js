cbeca18923044662cad90543b3032d193819dd8b
Even <Even72Max1@gmail.com>
2017-09-29 15:16:56 +0800

    bootstrap + router layout

--- src/Test/MyApp.js
+++ src/Test/MyApp.js
@@ -1,28 +1,94 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect, withRouter } from 'react-router-dom';
-import { ProgressBar } from 'react-bootstrap';
+import { Navbar,Nav,NavDropdown,NavItem,MenuItem } from 'react-bootstrap';
 
-////////////////////////////////////////////////////////////
-// 1. Click the public page
-// 2. Click the protected page
-// 3. Log in
-// 4. Click the back button, note the URL each time
+
+
 class MyApp extends Component{
   render(){
-    const now = 60;
-    return (
-      <div>
-       <ProgressBar now={now} label={`${now}%`} />
-    <ProgressBar striped bsStyle="success" now={40} label={'40%'} />
-    <ProgressBar striped bsStyle="info" now={20} />
-    <ProgressBar striped bsStyle="warning" now={60} />
-    <ProgressBar striped bsStyle="danger" now={80} />
-  </div>
-    );
 
+    return ( 
+      <Router>
+        <div>
+        <Navbar inverse collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#">React-Bootstrap</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} href="#"><Link to="/">Home</Link></NavItem>
+        <NavItem eventKey={2} href="#"><Link to="/about">About</Link></NavItem>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.3}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1} href="#">Link Right</NavItem>
+        <NavItem eventKey={2} href="#">Link Right</NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar> 
+  <Route exact path="/" component={Home}/>     
+  <Route path="/about" component={About}/>    
+        </div>
+  </Router>
+     
+      );
+      
   }
-
 }
 
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
   
   export default MyApp
